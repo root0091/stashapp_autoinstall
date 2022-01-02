c51d82c0dba1c59c38709ff6a926ce5a441c9152
root0091 <85199990+root0091@users.noreply.github.com>
2022-01-02 18:58:10 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -13,9 +13,9 @@ sleep 5
 export CHROME_BIN=/usr/bin/google-chrome
 export DISPLAY=:99.0
 apt install -y xvfb 
-sudo apt-get update
-sudo apt-get install -y libappindicator1 fonts-liberation libasound2 libgconf-2-4 libnspr4 libxss1 libnss3 xdg-utils
-sudo apt-get install -y python3 python3-pip python3-pil python3-requests
+apt-get update
+apt-get install -y libappindicator1 fonts-liberation libasound2 libgconf-2-4 libnspr4 libxss1 libnss3 xdg-utils
+apt-get install -y python3 python3-pip python3-pil python3-requests
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome*.deb
 apt --fix-broken -y install
